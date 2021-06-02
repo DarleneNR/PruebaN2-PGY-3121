58e2a12fbfbd248dcc0d4d5c48d899a21e540bea
Darlene Navarro <dar.navarror@duocuc.cl>
2021-06-01 23:39:27 -0400

    Login y Validación Lista

---
+++ JS/Index.js
@@ -0,0 +1,69 @@
+/* Validación con libreria */
+$(document).ready(function () {
+    /*$("#form-login").validate();*/
+    $("#submit").click(function(){
+        var str_user = $("#user").val().trim();
+        if (str_user == 'admin123') {
+            $("#mensaje1").fadeOut();
+            /*Redirección a segunda página*/
+            location.href = "Home.html";
+            return false;
+        }else {
+            $("#mensaje1").fadeIn(3);
+            return false;
+        }
+    });
+});
+
+$(document).ready(function () {
+    /*$("#form-login").validate();*/
+    $("#submit").click(function(){
+        var str_email = $("#email").val().trim();
+        if (str_email == 'admin@duoc.cl') {
+            $("#mensaje2").fadeOut();
+            /*Redirección a segunda página*/
+            location.href = "Home.html";
+            return false;
+        }else {
+            $("#mensaje2").fadeIn(3);
+            return false;
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function enviar() {
+    var str_user = document.getElementById("user").val().trim();
+    var str_email = document.getElementById("email").val().trim();
+    if (str_user == 'admin123' || str_email == 'admin@duoc.cl') {
+        location.href = "Home.html";
+    }
+};
+
+/* function enviar() {
+    location.href = "Home.html";
+}; */
